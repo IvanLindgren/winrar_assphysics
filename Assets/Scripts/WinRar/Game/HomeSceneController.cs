75f608459ef701d1e62fc878d5dfe4711783233f
damir <damir180501@mail.ru>
2023-11-27 03:46:27 +0300

    + First Final Commit

---
+++ Assets/Scripts/WinRar/Game/HomeSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace WinRar.Game
+{
+    public class HomeSceneController : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+        [SerializeField] private Trigger _finishTrigger;
+
+        private void Start()
+        {
+            _player.OnDead += Player_OnDead;
+            _finishTrigger.OnTriggerEnter += FinishTrigger_Enter;
+        }
+
+        private void Player_OnDead()
+        {
+            SceneManager.LoadScene("BadComics");
+        }
+
+        private void FinishTrigger_Enter(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+                SceneManager.LoadScene("FinishComicsScene");
+        }
+    }
+}
